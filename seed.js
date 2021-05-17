ce57dfe9b89dcf6237d3e4a201d89bfd3249e156
Rehan Thomas <66900370+RehanThomaspro@users.noreply.github.com>
2021-05-17 13:39:46 +0530

    Update seed.js

--- seed.js
+++ seed.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const Customer = require("./models/user");
 
-mongoose.connect("mongodb://127.0.0.1:27017/banking", { useUnifiedTopology: true, useNewUrlParser: true })
+mongoose.connect("mongodb://localhost:27017/banking", { useUnifiedTopology: true, useNewUrlParser: true })
     .then(() => {
         console.log("connected");
     })
